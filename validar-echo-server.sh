2b4f01f2b650b27dae232eb3c22b5ffdb0397eae
mslepowron <mslepowron@fi.uba.ar>
2025-08-25 17:28:41 -0300

    remove extra nc check

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -13,7 +13,7 @@ get_config() {
 SERVER_PORT=$(get_config "SERVER_PORT")
 SERVER_IP=$(get_config "SERVER_IP")
 
-RESPONSE=$(docker run --rm --network tp0_testing_net alpine:latest sh -c "apk add --no-cache netcat-openbsd >/dev/null 2>&1 && echo '$TEST_MESSAGE' | nc $SERVER_IP $SERVER_PORT")
+RESPONSE=$(docker run --rm --network tp0_testing_net alpine:latest sh -c "echo '$TEST_MESSAGE' | nc $SERVER_IP $SERVER_PORT")
 
 if [ "$RESPONSE" = "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
